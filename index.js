f439995a2f32a12310c6ae427d27840984c31a24
Wes Morgan <wesmorgan@icloud.com>
2020-01-16 16:48:03 -0700

    Add Cloudflare DNS support

--- index.js
+++ index.js
@@ -7,16 +7,27 @@ async function main() {
     const pinningService = core.getInput("pinning-service");
     const pinataAPIKey = core.getInput("pinata-api-key");
     const pinataSecretAPIKey = core.getInput("pinata-secret-api-key");
+    const cloudflareAPIEmail = core.getInput("cloudflare-api-email");
+    const cloudflareAPIKey = core.getInput("cloudflare-api-key");
+    const cloudflareZone = core.getInput("cloudflare-zone");
+    const cloudflareRecord = core.getInput("cloudflare-record");
 
-    core.info(`Deploying to ${pinningService}`);
+    core.info(`Deploying to ${pinningService} & updating Cloudflare DNS`);
 
     const ipfsHash = await deploy({
       publicDirPath: deployDir,
       remotePinners: [pinningService],
+      dnsProviders: ['cloudflare'],
       credentials: {
         pinata: {
           apiKey: pinataAPIKey,
           secretApiKey: pinataSecretAPIKey
+        },
+        cloudflare: {
+          apiEmail: cloudflareAPIEmail,
+          apiKey: cloudflareAPIKey,
+          zone: cloudflareZone,
+          record: cloudflareRecord
         }
       },
       open: false,
